1be916849668eb7c2a5ab32600a0c083d0773922
Enzo de Melo Bolognani <enzombolognani@gmail.com>
2024-07-03 18:55:20 -0300

    Add files via upload

---
+++ codigo/Pagina-de-Usuario/public/assets/scripts/script.js
@@ -0,0 +1,38 @@
+function loadUserProfile(cpf) {
+    fetch('/paciente')
+        .then(response => response.json())
+        .then(data => {
+            const user = data.find(paciente => paciente.cpf == cpf);
+            if (user) {
+                document.getElementById('user-photo').src = "assets/images/" + user.foto;
+                document.getElementById('user-name').innerText = user.nome;
+                document.getElementById('user-email').innerText = user.email;
+                document.getElementById('user-age').innerText = user.idade + ' anos';
+                document.getElementById('user-gender').innerText = user.sexo;
+
+                const ratingContainer = document.getElementById('user-rating');
+                ratingContainer.innerHTML = '';
+                const rating = user.avaliacao;
+                for (let i = 1; i <= 5; i++) {
+                    const starImg = document.createElement('img');
+                    if (rating >= i) {
+                        starImg.src = 'assets/images/star-icon.png';
+                    } else if (rating >= i - 0.5) {
+                        starImg.src = 'assets/images/star-half-icon.png';
+                    } else {
+                        starImg.src = 'assets/images/star-empty-icon.png';
+                    }
+                    starImg.alt = 'Star';
+                    ratingContainer.appendChild(starImg);
+                }
+            } else {
+                console.error('Paciente não encontrado');
+            }
+        })
+        .catch(error => console.error('Erro ao carregar dados do JSON:', error));
+}
+
+window.onload = function() {
+    const cpf = 12345678900; // Substitua pelo CPF desejado
+    loadUserProfile(cpf);
+};
